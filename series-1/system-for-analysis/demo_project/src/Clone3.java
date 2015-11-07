f1a483933cf1c32eea306659ed4ac25a219178f0
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-11-07 15:10:53 +0100

    Add unit tests on the demo project.

--- series-1/system-for-analysis/demo_project/src/Clone3.java
+++ series-1/system-for-analysis/demo_project/src/Clone3.java
@@ -19,4 +19,25 @@ class Clone3 {
 		System.out.print("6");
 		System.out.print("7");
 	}
+	
+
+	public void clonePrint3() {
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
+		System.out.print("4");
+		System.out.print("5");
+		System.out.print("6");
+		System.out.print("7");
+	}	
+
+	public void clonePrint4() {
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
+		System.out.print("4");
+		System.out.print("5");
+		System.out.print("6");
+		System.out.print("7");
+	}
 }
