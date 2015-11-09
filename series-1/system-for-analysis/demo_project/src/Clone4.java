ae078503fd43e97c9d09f5344b775159b5ddc443
Theologos_Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-11-09 15:26:55 +0100

    Add new clone

---
+++ series-1/system-for-analysis/demo_project/src/Clone4.java
@@ -0,0 +1,29 @@
+class Clone4 {
+	public void clonePrint1() {
+		System.out.print("10");
+		System.out.print("20");
+		System.out.print("30");
+		System.out.print("40");
+		System.out.print("50");
+		System.out.print("60");
+	}
+	
+	public void clonePrint2() {
+		System.out.print("10");
+		System.out.print("20");
+		System.out.print("30");
+		System.out.print("40");
+		System.out.print("50");
+		System.out.print("60");
+		System.out.print("70");
+	}
+	
+	public void clonePrint3() {
+		System.out.print("20");
+		System.out.print("30");
+		System.out.print("40");
+		System.out.print("50");
+		System.out.print("60");
+		System.out.print("70");
+	}
+}
