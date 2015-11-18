52ff6a7f6bbc666baf28a7ae7b5466afad39fb2e
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-11-18 22:34:21 +0100

    small changes in example clones

--- series-1/system-for-analysis/clones_demo/src/Clone1.java
+++ series-1/system-for-analysis/clones_demo/src/Clone1.java
@@ -1,6 +1,6 @@
 class Clone1 {
 	public boolean clone1() {
-		int a = 10;
+		int a = 50;
 		if (a > 10)
 			return true;
 		else 
--- series-1/system-for-analysis/clones_demo/src/Clone2.java
+++ series-1/system-for-analysis/clones_demo/src/Clone2.java
@@ -1,6 +1,6 @@
 class Clone2 {
 	public boolean clone2() {
-		int a = 10;
+		int a = 50;
 		if (a > 10)
 			return true;
 		else 
