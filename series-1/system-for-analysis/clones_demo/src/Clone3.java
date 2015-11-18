60caf2315442eec86bac1ee27d72d4dbbed91dd9
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-11-18 19:59:26 +0100

    Add clones_demo project to systems-for-analysis

---
+++ series-1/system-for-analysis/clones_demo/src/Clone3.java
@@ -0,0 +1,45 @@
+class Clone3 {
+	public void clonePrint1() {
+		System.out.print("1");
+		// Just a comment, still duplicate
+		System.out.print("2");
+		System.out.print("3");
+		System.out.print("4");
+		System.out.print("5");
+		System.out.print("6");
+		System.out.print("7");
+        System.out.print("7");
+	}
+	
+	public void clonePrint2() {
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
+		System.out.print("4");
+		System.out.print("5");
+		System.out.print("6");
+		System.out.print("7");
+        System.out.print("7");
+	}
+	
+
+	public void clonePrint3() {
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
+		System.out.print("4");
+		System.out.print("5");
+		System.out.print("6");
+		System.out.print("7");
+	}	
+
+	public void clonePrint4() {
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
+		System.out.print("4");
+		System.out.print("5");
+		System.out.print("6");
+		System.out.print("7");
+	}
+}
