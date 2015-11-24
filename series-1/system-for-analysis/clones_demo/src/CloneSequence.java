f2f39b4af72fffdd2686d2fe06c0236888575d66
Theologos_Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-11-24 11:07:00 +0100

    Add clone sequence example

---
+++ series-1/system-for-analysis/clones_demo/src/CloneSequence.java
@@ -0,0 +1,25 @@
+class CloneSequence {
+	int x;
+	int a;
+	int b;
+	int c;
+	int w;
+	int y;
+	int i;
+	
+	void f () {
+		x=0;
+		a=1; // sequence start
+		b=2;
+		c=3; // sequence end
+		w=4;
+	}
+	
+	void g () {
+		y=2;
+		a=1; // sequence start
+		b=2;
+		c=3; // sequence end
+		i=5;
+	}
+}
