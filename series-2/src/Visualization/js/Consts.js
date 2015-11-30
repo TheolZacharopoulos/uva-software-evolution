70a77e0076d88c56deb6005c6ce51f3b3190283f
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-11-30 21:01:34 +0100

    Visualization add links and nodes

---
+++ series-2/src/Visualization/js/Consts.js
@@ -0,0 +1,9 @@
+const DATA_FILE = "demo_project/readme-flare-imports.json";
+const DIAMETER = 800;
+const DEGREES = 360;
+const TENSION = .85;
+
+// size of the wheel
+const diameter = DIAMETER,
+      radius = diameter / 2,
+      innerRadius = radius - 120;
